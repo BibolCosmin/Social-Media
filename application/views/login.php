6b603e1ebee78fabaedc3012e1d4d6ae3c033bce
BibolCosmin <cosmin.bibol@yahoo.com>
2017-04-05 12:55:38 +0300

    ssss

---
+++ application/views/login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/loginstyle.css">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Login</title>
+
+    <!-- Bootstrap -->
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    
+  </head>
+  <body>
+ <div class="container-fluid">
+     <div class="row">
+         <div class="overlay"><h1>Overlay Text</h1></div>
+    <div id="theCarousel" class="carousel slide" data-ride="carousel">
+            
+            <div class="carousel-inner">
+                <div class="item active" >
+                    <div class ="slide1">
+                        <img class="img-responsive" src="http://localhost/Project/assets/photos\z-w.jpg"  alt="...">
+                        
+                    </div>
+
+                </div>
+                <div class="item ">
+                    <div class="slide1">
+                        <img class="img-responsive" src="http://localhost/Project/assets/photos\z-w.jpg"  alt="...">
+                    </div>
+
+
+                </div>
+                <div class="item">
+                    <div class="slide1">
+                        <img class="img-responsive" src="http://localhost/Project/assets/photos/nature-best-wallpapers-images-landscape-195743.jpg"  alt="...">
+                    </div>
+                    
+                </div>
+                
+                
+            </div>
+            
+        </div>
+     </div>
+      </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery-3.2.0.min.js" type="text/javascript"></script>
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+ 
+        
+   
+  </body>
+</html>
